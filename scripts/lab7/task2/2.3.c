6d690127ed3897dd28533ab9a6970f191434cf7e
Челнокова Александра Витальевна <chelalex1511@gmail.com>
2021-12-14 23:02:29 +0300

    2.3 - 10 дочерних процессов

---
+++ scripts/lab7/task2/2.3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+  int pid = fork();
+  // определить, в каком процессе мы находимся, помогает переменная pid
+  for (int i = 0; i < 10; i++) {
+  	if (pid > 0) {
+		fork();
+	}
+  }
+  sleep(3);
+  return 0;
+}
